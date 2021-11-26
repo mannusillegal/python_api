22e1354b36c67fdd925a70b66fb578fb6958083b
mannusillegal <suraj.singh007@hotmail.com>
2021-11-26 21:40:16 +0530

    new files

---
+++ app.py
@@ -0,0 +1,21 @@
+import requests
+import config
+
+
+url = "https://api.yelp.com/v3/businesses/search"
+headers = {
+    "Authorization": "Bearer " + config.api_key
+}
+params = {
+    "term": "barber",
+    "location": "NYC"
+}
+response = requests.get(url, headers=headers, params=params)
+
+businesses = response.json()["businesses"]
+# [item for item in list]
+names = [business["name"]
+         for business in businesses if business["rating"] > 4.5]
+
+
+print(names)
